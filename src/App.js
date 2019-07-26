6f292e82fc8695692e10f18ebe4be36a372c7bc8
14bscsbanwer <14bscsbanwer@gmail.com>
2019-07-27 03:24:09 +0500

    testing safepay

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ PureComponent } from 'react';
+import SafepayButton from './safepay-button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default class App extends PureComponent {
+  render() {
+    return (
+        <div className="demo">
+          <div className="sp-button">
+            <SafepayButton />
+          </div>
+        </div>  
+    )
+  }
+ }
---
+++ src/safepay-react.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let SafepayCheckoutComponent = window.safepay.Button.driver('react', {
+ React: React,
+ ReactDOM: ReactDOM
+});
+
+export default SafepayCheckoutComponent
